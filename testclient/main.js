4e29119195f6a39ef8ae35f1f927220e1b6da9ae
Neal Tucker <ntucker@august20th.com>
2020-07-26 16:51:01 -0700

    Include local test client for debugscrewery

---
+++ testclient/main.js
@@ -0,0 +1,23 @@
+const Looper = require("./YoutubeLooper.js");
+
+var elements = {
+    videoContainer: document.getElementById("videocontainer"),
+    urlinput: document.getElementById("urlinput"),
+    buttons: {
+        start: document.getElementById("button_start"),
+        end: document.getElementById("button_end"),
+        load: document.getElementById("button_load")
+    }
+}
+
+const l = new Looper(elements.videoContainer, null);
+elements.buttons.start.addEventListener("click", (e) => {
+    l.SetStart();
+});
+elements.buttons.end.addEventListener("click", (e) => {
+    l.SetEnd();
+});
+elements.buttons.load.addEventListener("click", (e) => {
+    l.LoadURL(elements.urlinput.value);
+});
+window.l = l;
